177f250a3bb7862d403c6a0a7aa11f2a6c6d820b
Duodu Randy <duodurandy19@gmail.com>
2021-05-21 18:22:50 +0000

    Deploy app on heroku

--- content/__init__.py
+++ content/__init__.py
@@ -11,7 +11,7 @@ username = os.environ.get('MONGO_USER')
 pswd = os.environ.get('MONGO_PSWD')
 db_name = os.environ.get('MONGO_DBNAME')
 uri = f"mongodb+srv://{username}:{pswd}@agms01.vtxt7.mongodb.net/{db_name}?retryWrites=true&w=majority"
-mongo_client = MongoClient()
+mongo_client = MongoClient(uri)
 mongo = mongo_client.get_database(f"{db_name}")
 
 # Config Mail
@@ -24,6 +24,5 @@ app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['MAIL_MAX_EMAILS'] = 1000
 
 mail = Mail(app)
-
 
 from . import routes
